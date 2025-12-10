b0ae730d79f22b71ff8cd1eb35d56e0f79f6ded3
suwashshrestha <suwashshrestha5536@gmail.com>
2025-12-10 16:27:54 +0545

    push

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
-  }
-})
+    host: true,      // same as 0.0.0.0
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+  },
+});
